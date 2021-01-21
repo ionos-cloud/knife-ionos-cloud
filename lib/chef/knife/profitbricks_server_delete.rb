12a5ae56965de809671269104249b4034a3cb238
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-01-21 16:59:53 +0200

    server delete

--- lib/chef/knife/profitbricks_server_delete.rb
+++ lib/chef/knife/profitbricks_server_delete.rb
@@ -10,28 +10,33 @@ class Chef
       option :datacenter_id,
              short: '-D DATACENTER_ID',
              long: '--datacenter-id DATACENTER_ID',
-             description: 'Name of the data center',
-             proc: proc { |datacenter_id| Chef::Config[:knife][:datacenter_id] = datacenter_id }
+             description: 'Name of the data center'
 
       def run
-        connection
+        server_api = Ionoscloud::ServerApi.new(api_client)
+
         @name_args.each do |server_id|
           begin
-            server = ProfitBricks::Server.get(Chef::Config[:knife][:datacenter_id], server_id)
-          rescue Excon::Errors::NotFound
+            server = server_api.datacenters_servers_find_by_id(config[:datacenter_id], server_id, default_opts)
+          rescue Ionoscloud::ApiError => err
+            raise err unless err.code == 404
             ui.error("Server ID #{server_id} not found. Skipping.")
             next
           end
 
           msg_pair('ID', server.id)
-          msg_pair('Name', server.properties['name'])
-          msg_pair('Cores', server.properties['cores'])
-          msg_pair('RAM', server.properties['ram'])
-          msg_pair('Availability Zone', server.properties['availabilityZone'])
+          msg_pair('Name', server.properties.name)
+          msg_pair('Cores', server.properties.cores)
+          msg_pair('RAM', server.properties.ram)
+          msg_pair('Availability Zone', server.properties.availability_zone)
 
-          confirm('Do you really want to delete this server')
+          begin
+            confirm('Do you really want to delete this server')
+          rescue SystemExit => exc
+            next
+          end
 
-          server.delete
+          server_api.datacenters_servers_delete(config[:datacenter_id], server_id, default_opts)
           ui.warn("Deleted server #{server.id}")
         end
       end
