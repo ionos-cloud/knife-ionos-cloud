79d974c546242e3f76db35bcf59339c099ce6e20
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-01-22 16:44:37 +0200

    volume detach

--- lib/chef/knife/profitbricks_volume_attach.rb
+++ lib/chef/knife/profitbricks_volume_attach.rb
@@ -20,25 +20,20 @@ class Chef
       def run
         validate_required_params(%i(datacenter_id server_id), config)
         server_api = Ionoscloud::ServerApi.new(api_client)
-        volume_api = Ionoscloud::VolumeApi.new(api_client)
 
         @name_args.each do |volume_id|
           begin
-            volume = volume_api.datacenters_volumes_find_by_id(
-              config[:datacenter_id], volume_id, default_opts,
+            server_api.datacenters_servers_volumes_post(
+              config[:datacenter_id],
+              config[:server_id],
+              {'id' => volume_id},
+              default_opts,
             )
           rescue Ionoscloud::ApiError => err
             raise err unless err.code == 404
-            ui.error("Volume ID #{datacenter_id} not found. Skipping.")
+            ui.error("Volume ID #{volume_id} not found. Skipping.")
             next
           end
-          puts volume.to_hash
-          server_api.datacenters_servers_volumes_post(
-            config[:datacenter_id],
-            config[:server_id],
-            {'id' => volume_id},
-            default_opts)
-
           ui.msg("Volume #{volume_id} attached to server")
         end
       end
--- lib/chef/knife/profitbricks_volume_detach.rb
+++ lib/chef/knife/profitbricks_volume_detach.rb
@@ -10,8 +10,7 @@ class Chef
       option :datacenter_id,
              short: '-D DATACENTER_ID',
              long: '--datacenter-id DATACENTER_ID',
-             description: 'The ID of the data center',
-             proc: proc { |datacenter_id| Chef::Config[:knife][:datacenter_id] = datacenter_id }
+             description: 'The ID of the data center'
 
       option :server_id,
              short: '-S SERVER_ID',
@@ -19,25 +18,41 @@ class Chef
              description: 'The ID of the server'
 
       def run
-        connection
-        validate_required_params(%i(datacenter_id server_id), Chef::Config[:knife])
+        validate_required_params(%i(datacenter_id server_id), config)
+        server_api = Ionoscloud::ServerApi.new(api_client)
         @name_args.each do |volume_id|
           begin
-            volume = ProfitBricks::Volume.get(Chef::Config[:knife][:datacenter_id], nil, volume_id)
-          rescue Excon::Errors::NotFound
+            volume = server_api.datacenters_servers_volumes_find_by_id(
+              config[:datacenter_id],
+              config[:server_id],
+              volume_id,
+              default_opts,
+            )          
+          rescue Ionoscloud::ApiError => err
+            raise err unless err.code == 404
             ui.error("Volume ID #{volume_id} not found. Skipping.")
             next
           end
 
           msg_pair('ID', volume.id)
-          msg_pair('Name', volume.properties['name'])
-          msg_pair('Size', volume.properties['size'])
-          msg_pair('Bus', volume.properties['bus'])
-          msg_pair('Device Number', volume.properties['deviceNumber'])
+          msg_pair('Name', volume.properties.name)
+          msg_pair('Size', volume.properties.size)
+          msg_pair('Bus', volume.properties.bus)
+          msg_pair('Device Number', volume.properties.device_number)
 
-          confirm('Do you really want to detach this volume')
+          begin
+            confirm('Do you really want to detach this volume')
+          rescue SystemExit => exc
+            next
+          end
 
-          volume.detach(Chef::Config[:knife][:server_id])
+          server_api.datacenters_servers_volumes_delete(
+            config[:datacenter_id],
+            config[:server_id],
+            volume.id,
+            default_opts,
+          )
+
           ui.msg("Detaching volume #{volume_id} from server")
         end
       end
