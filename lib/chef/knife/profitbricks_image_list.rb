01143f7319d3012333e670076b42f1ce2e20104f
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-01-22 17:02:03 +0200

    images list

--- lib/chef/knife/profitbricks_image_list.rb
+++ lib/chef/knife/profitbricks_image_list.rb
@@ -17,15 +17,18 @@ class Chef
           ui.color('Size', :bold),
           ui.color('Public', :bold)
         ]
+        image_api = Ionoscloud::ImageApi.new(api_client)
+        opts = default_opts.update({
+          :depth => 1,
+        })
 
-        connection
-        ProfitBricks::Image.list.each do |image|
+        image_api.images_get(opts).items.each do |image|
           image_list << image.id
-          image_list << image.properties['name']
-          image_list << image.properties['description']
-          image_list << image.properties['location']
-          image_list << image.properties['size'].to_s
-          image_list << image.properties['public'].to_s
+          image_list << image.properties.name
+          image_list << image.properties.description
+          image_list << image.properties.location
+          image_list << image.properties.size.to_s
+          image_list << image.properties.public.to_s
         end
 
         puts ui.list(image_list, :uneven_columns_across, 6)
