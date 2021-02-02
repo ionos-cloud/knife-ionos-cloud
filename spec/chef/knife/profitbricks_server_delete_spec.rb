98e99cf390d4a279bbf29828d32173b6ecb24f8d
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-02-02 15:29:41 +0200

    server create delete list

--- spec/chef/knife/profitbricks_server_delete_spec.rb
+++ spec/chef/knife/profitbricks_server_delete_spec.rb
@@ -7,56 +7,60 @@ describe Chef::Knife::ProfitbricksServerDelete do
   subject { Chef::Knife::ProfitbricksServerDelete.new }
 
   before :each do
+    Ionoscloud.configure do |config|
+      config.username = ENV['IONOS_USERNAME']
+      config.password = ENV['IONOS_PASSWORD']
+    end
+
+    @datacenter, _, headers  = Ionoscloud::DataCenterApi.new.datacenters_post_with_http_info({
+      properties: {
+        name: 'Chef test Datacenter',
+        description: 'Chef test datacenter',
+        location: 'de/fra',
+      },
+    })
+    Ionoscloud::ApiClient.new.wait_for { is_done? get_request_id headers }
+
+    @server, _, headers  = Ionoscloud::ServerApi.new.datacenters_servers_post_with_http_info(
+      @datacenter.id,
+      {
+        properties: {
+          name: 'Chef test Server',
+          ram: 1024,
+          cores: 1,
+          availabilityZone: 'ZONE_1',
+          cpuFamily: 'INTEL_SKYLAKE',
+        },
+      },
+    )
+    Ionoscloud::ApiClient.new.wait_for { is_done? get_request_id headers }
+
     allow(subject).to receive(:puts)
-    subject.config[:yes] = true
-
-    {
-      name: 'Chef Test',
-      public: 'true'
-    }.each do |key, value|
-      Chef::Config[:knife][key] = value
-    end
-
-    ProfitBricks.configure do |config|
-      config.username = Chef::Config[:knife][:profitbricks_username]
-      config.password = Chef::Config[:knife][:profitbricks_password]
-      config.url = Chef::Config[:knife][:profitbricks_url]
-      config.debug = Chef::Config[:knife][:profitbricks_debug] || false
-      config.global_classes = false
-    end
-
-    @datacenter = ProfitBricks::Datacenter.create(name: 'Chef test',
-                                                  description: 'Chef test datacenter',
-                                                  location: 'us/las')
-    @datacenter.wait_for { ready? }
-
-    @server = ProfitBricks::Server.create(@datacenter.id, name: 'Chef Test',
-                                                          ram: 1024,
-                                                          cores: 1,
-                                                          availabilityZone: 'ZONE_1',
-                                                          cpuFamily: 'INTEL_XEON')
-    @server.wait_for { ready? }
-
-    Chef::Config[:knife][:datacenter_id] = @datacenter.id
-
-    subject.name_args = [@server.id]
   end
 
   after :each do
-    ProfitBricks.configure do |config|
-      config.username = Chef::Config[:knife][:profitbricks_username]
-      config.password = Chef::Config[:knife][:profitbricks_password]
-      config.url = Chef::Config[:knife][:profitbricks_url]
-      config.debug = Chef::Config[:knife][:profitbricks_debug] || false
-      config.global_classes = false
-    end
-
-    @datacenter.delete
-    @datacenter.wait_for { ready? }
+    _, _, headers  = Ionoscloud::DataCenterApi.new.datacenters_delete_with_http_info(@datacenter.id)
   end
 
   describe '#run' do
-    it 'should delete a server' do
+    it 'should delete a server when yes' do
+      {
+        profitbricks_username: ENV['IONOS_USERNAME'],
+        profitbricks_password: ENV['IONOS_PASSWORD'],
+        datacenter_id: @datacenter.id,
+      }.each do |key, value|
+        subject.config[key] = value
+      end
+
+      subject.name_args = [@server.id]
+      subject.config[:yes] = true
+
+      expect(subject).to receive(:puts).with("ID: #{@server.id}")
+      expect(subject).to receive(:puts).with("Name: #{@server.properties.name}")
+      expect(subject).to receive(:puts).with("Cores: #{@server.properties.cores}")
+      expect(subject).to receive(:puts).with("RAM: #{@server.properties.ram}")
+      expect(subject).to receive(:puts).with("Availability Zone: #{@server.properties.availability_zone}")
+
       subject.run
     end
   end
--- spec/chef/knife/profitbricks_server_list_spec.rb
+++ spec/chef/knife/profitbricks_server_list_spec.rb
@@ -7,58 +7,56 @@ describe Chef::Knife::ProfitbricksServerList do
   subject { Chef::Knife::ProfitbricksServerList.new }
 
   before :each do
-    {
-      name: 'Chef Test',
-      public: 'true'
-    }.each do |key, value|
-      Chef::Config[:knife][key] = value
+    Ionoscloud.configure do |config|
+      config.username = ENV['IONOS_USERNAME']
+      config.password = ENV['IONOS_PASSWORD']
     end
 
-    ProfitBricks.configure do |config|
-      config.username = Chef::Config[:knife][:profitbricks_username]
-      config.password = Chef::Config[:knife][:profitbricks_password]
-      config.url = Chef::Config[:knife][:profitbricks_url]
-      config.debug = Chef::Config[:knife][:profitbricks_debug] || false
-      config.global_classes = false
-    end
+    @datacenter, _, headers  = Ionoscloud::DataCenterApi.new.datacenters_post_with_http_info({
+      properties: {
+        name: 'Chef test Datacenter',
+        description: 'Chef test datacenter',
+        location: 'de/fra',
+      },
+    })
+    Ionoscloud::ApiClient.new.wait_for { is_done? get_request_id headers }
 
-    @datacenter = ProfitBricks::Datacenter.create(name: 'Chef test',
-                                                  description: 'Chef test datacenter',
-                                                  location: 'us/las')
-    @datacenter.wait_for { ready? }
+    @server, _, headers  = Ionoscloud::ServerApi.new.datacenters_servers_post_with_http_info(
+      @datacenter.id,
+      {
+        properties: {
+          name: 'Chef test Server',
+          ram: 1024,
+          cores: 1,
+          availabilityZone: 'ZONE_1',
+          cpuFamily: 'INTEL_SKYLAKE',
+        },
+      },
+    )
+    Ionoscloud::ApiClient.new.wait_for { is_done? get_request_id headers }
 
-    @lan = ProfitBricks::LAN.create(@datacenter.id, name: 'Chef Test',
-                                                    public: 'true')
-    @lan.wait_for { ready? }
-
-    @server = ProfitBricks::Server.create(@datacenter.id, name: 'Chef Test',
-                                                          ram: 1024,
-                                                          cores: 1,
-                                                          availabilityZone: 'ZONE_1',
-                                                          cpuFamily: 'INTEL_XEON')
-    @server.wait_for { ready? }
-
-    Chef::Config[:knife][:datacenter_id] = @datacenter.id
-
+    @server = Ionoscloud::ServerApi.new.datacenters_servers_find_by_id(@datacenter.id, @server.id)
+  
     allow(subject).to receive(:puts)
   end
 
   after :each do
-    ProfitBricks.configure do |config|
-      config.username = Chef::Config[:knife][:profitbricks_username]
-      config.password = Chef::Config[:knife][:profitbricks_password]
-      config.url = Chef::Config[:knife][:profitbricks_url]
-      config.debug = Chef::Config[:knife][:profitbricks_debug] || false
-      config.global_classes = false
-    end
-
-    @datacenter.delete
-    @datacenter.wait_for { ready? }
+    _, _, headers  = Ionoscloud::DataCenterApi.new.datacenters_delete_with_http_info(@datacenter.id)
   end
 
   describe '#run' do
     it 'should output the column headers' do
-      expect(subject).to receive(:puts).with(/^ID\s+Name\s+Cores\s+RAM\s+ Availability Zone\s+VM State\s+Boot Volume\s+Boot CDROM\s*$/)
+      {
+        profitbricks_username: ENV['IONOS_USERNAME'],
+        profitbricks_password: ENV['IONOS_PASSWORD'],
+        datacenter_id: @datacenter.id,
+      }.each do |key, value|
+        subject.config[key] = value
+      end
+
+      expect(subject).to receive(:puts).with(
+        /^ID\s+Name\s+Cores\s+RAM\s+Availability Zone\s+VM State\s+Boot Volume\s+Boot CDROM\s*$\n#{@server.id}\s+#{@server.properties.name}\s+#{@server.properties.cores}\s+#{@server.properties.ram}\s+#{@server.properties.availability_zone}\s+#{@server.properties.vm_state}\s+#{(@server.properties.boot_volume == nil ? '' : @server.properties.boot_volume.id)}\s+#{(@server.properties.boot_cdrom == nil ? '' : @server.properties.boot_cdrom.id)}\s*$/,
+      )
       subject.run
     end
   end
