3a1d24d49ddd47e4bdbe9cc42d09100e1dc40394
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-02-03 13:27:27 +0200

    image list

--- spec/chef/knife/profitbricks_image_list_spec.rb
+++ spec/chef/knife/profitbricks_image_list_spec.rb
@@ -4,16 +4,20 @@ require 'profitbricks_image_list'
 Chef::Knife::ProfitbricksImageList.load_deps
 
 describe Chef::Knife::ProfitbricksImageList do
-  let(:image_list) { Chef::Knife::ProfitbricksImageList.new }
-
-  before :each do
-    allow(image_list).to receive(:puts)
-  end
+  subject { Chef::Knife::ProfitbricksImageList.new }
 
   describe '#run' do
     it 'should output the column headers' do
-      expect(image_list).to receive(:puts).with(/^ID\s+Name\s+Description\s+Location\s+Size\s+Public\s*$/)
-      image_list.run
+      {
+        profitbricks_username: ENV['IONOS_USERNAME'],
+        profitbricks_password: ENV['IONOS_PASSWORD'],
+      }.each do |key, value|
+        subject.config[key] = value
+      end
+      allow(subject).to receive(:puts)
+
+      expect(subject).to receive(:puts).with(/^ID\s+Name\s+Description\s+Location\s+Size\s+Public\s*$/)
+      subject.run
     end
   end
 end
