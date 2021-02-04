2a36fef1460c1c9eb47b8fa2fa9b4569bfea3ff0
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-02-04 11:13:09 +0200

    updated datacenter delete

--- spec/chef/knife/profitbricks_datacenter_delete_spec.rb
+++ spec/chef/knife/profitbricks_datacenter_delete_spec.rb
@@ -21,6 +21,10 @@ describe Chef::Knife::ProfitbricksDatacenterDelete do
     })
     Ionoscloud::ApiClient.new.wait_for { is_done? get_request_id headers }
 
+    @datacenter = Ionoscloud::DataCenterApi.new.datacenters_find_by_id(@datacenter.id)
+
+    Ionoscloud::ApiClient.new.wait_for { is_done? get_request_id headers }
+
     allow(subject).to receive(:puts)
     allow(subject.ui).to receive(:warn)
     allow(subject).to receive(:confirm)
@@ -45,18 +49,51 @@ describe Chef::Knife::ProfitbricksDatacenterDelete do
       subject.config[:yes] = true
       subject.name_args = [@datacenter.id]
 
+      allow(subject.ui).to receive(:warn).with(
+        /Deleted data center #{@datacenter.id}. Request ID: (\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12})\b/,
+      ) do |arg|
+        @request_id = arg.split('Request ID: ').last
+      end
+
+      expect(subject).to receive(:puts).with("ID: #{@datacenter.id}")
       expect(subject).to receive(:puts).with("Name: #{@datacenter.properties.name}")
       expect(subject).to receive(:puts).with("Description: #{@datacenter.properties.description}")
       expect(subject).to receive(:puts).with("Location: #{@datacenter.properties.location}")
+      expect(subject).to receive(:puts).with("Version: #{@datacenter.properties.version}")
+
       subject.run
 
-      sleep(1)
-      begin
-        Ionoscloud::DataCenterApi.new.datacenters_find_by_id(@datacenter.id)
-      rescue Exception => err
-        expect(err).to be_a(Ionoscloud::ApiError)
-        expect(err.code).to eq(404)
+      raise Exception('No Request ID found.') unless @request_id
+
+      request = Ionoscloud::RequestApi.new.requests_status_get(@request_id)
+
+      expect(request.metadata.status).to eq('QUEUED')
+      expect(request.metadata.message).to eq('Request has been queued')
+      expect(request.metadata.targets.length).to eq(1)
+      expect(request.metadata.targets.first.target.type).to eq('datacenter')
+      expect(request.metadata.targets.first.target.id).to eq(@datacenter.id)
+
+      Ionoscloud::ApiClient.new.wait_for { is_done? @request_id }
+      
+      expect{ Ionoscloud::DataCenterApi.new.datacenters_find_by_id(@datacenter.id) }.to raise_error(Ionoscloud::ApiError) do |error|
+        expect(error.code).to eq(404)
       end
+    end
+
+    it 'should print a message when wrong ID' do
+      {
+        profitbricks_username: ENV['IONOS_USERNAME'],
+        profitbricks_password: ENV['IONOS_PASSWORD'],
+      }.each do |key, value|
+        subject.config[key] = value
+      end
+      datacenter_id = 123
+  
+      subject.config[:yes] = false
+      subject.name_args = [datacenter_id]
+
+      expect(subject.ui).to receive(:error).with("Data center ID #{datacenter_id} not found. Skipping.")
+      subject.run
     end
   end
 end
