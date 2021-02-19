ed65e830be8f90a4dd1bf08d8b9f79752eb65472
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-02-19 17:14:09 +0200

    pcc

---
+++ lib/chef/knife/ionoscloud_pcc_delete.rb
@@ -0,0 +1,57 @@
+require_relative 'ionoscloud_base'
+
+class Chef
+  class Knife
+    class IonoscloudPccDelete < Knife
+      include Knife::IonoscloudBase
+
+      banner 'knife ionoscloud pcc delete PCC_ID [PCC_ID]'
+
+      attr_reader :description, :required_options
+      
+      def initialize(args = [])
+        super(args)
+        @description =
+        'Deletes a Private Cross-Connect.'
+        @required_options = [:ionoscloud_username, :ionoscloud_password]
+      end
+
+      def run
+        $stdout.sync = true
+        validate_required_params(@required_options, config)
+
+        pcc_api = Ionoscloud::PrivateCrossConnectApi.new(api_client)
+
+        @name_args.each do |pcc_id|
+          begin
+            pcc = pcc_api.pccs_find_by_id(pcc_id)
+          rescue Ionoscloud::ApiError => err
+            raise err unless err.code == 404
+            ui.error("PCC ID #{pcc_id} not found. Skipping.")
+            next
+          end
+
+          peers = pcc.properties.peers.map {|peer| peer.id}
+          datacenters = pcc.properties.connectable_datacenters.map {|datacenter| datacenter.id}
+
+          msg_pair('ID', pcc.id)
+          msg_pair('Name', pcc.properties.name)
+          msg_pair('Description', pcc.properties.description)
+          msg_pair('Peers', peers.to_s)
+          msg_pair('Datacenters', datacenters.to_s)
+
+          puts "\n"
+
+          begin
+            confirm('Do you really want to delete this PCC')
+          rescue SystemExit => exc
+            next
+          end
+
+          _, _, headers = pcc_api.pccs_delete_with_http_info(pcc_id)
+          ui.warn("Deleted PCC #{pcc.id}. Request ID: #{get_request_id headers}")
+        end
+      end
+    end
+  end
+end
---
+++ lib/chef/knife/ionoscloud_pcc_list.rb
@@ -0,0 +1,41 @@
+require_relative 'ionoscloud_base'
+
+class Chef
+  class Knife
+    class IonoscloudPccList < Knife
+      include Knife::IonoscloudBase
+
+      banner 'knife ionoscloud pcc list'
+
+      attr_reader :description, :required_options
+      
+      def initialize(args = [])
+        super(args)
+        @description =
+        'Lists all Private Cross-Connect instances.'
+        @required_options = [:ionoscloud_username, :ionoscloud_password]
+      end
+
+      def run
+        $stdout.sync = true
+        validate_required_params(@required_options, config)
+
+        pcc_list = [
+          ui.color('ID', :bold),
+          ui.color('Name', :bold),
+          ui.color('Description', :bold),
+        ]
+
+        pcc_api = Ionoscloud::PrivateCrossConnectApi.new(api_client)
+
+        pcc_api.pccs_get({ depth: 1 }).items.each do |pcc|
+          pcc_list << pcc.id
+          pcc_list << pcc.properties.name
+          pcc_list << pcc.properties.description || ''
+        end
+
+        puts ui.list(pcc_list, :uneven_columns_across, 3)
+      end
+    end
+  end
+end
