93f82894601a423397d2b891486982af35a6e57f
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-02-25 15:35:10 +0200

    fixed snapshot create

--- lib/chef/knife/ionoscloud_snapshot_create.rb
+++ lib/chef/knife/ionoscloud_snapshot_create.rb
@@ -16,21 +16,6 @@ class Chef
               short: '-V VOLUME_ID',
               long: '--volume VOLUME_ID',
               description: 'ID of the Volume'
-
-      option :name,
-              short: '-n NAME',
-              long: '--name NAME',
-              description: 'Name of the data center'
-
-      option :description,
-              short: '-d DESCRIPTION',
-              long: '--description DESCRIPTION',
-              description: 'Description of the data center'
-
-      option :licence_type,
-              short: '-l LICENCE_TYPE',
-              long: '--licence-type LICENCE_TYPE',
-              description: 'Set to one of the values: [WINDOWS, WINDOWS2016, LINUX, OTHER, UNKNOWN]'
       
       attr_reader :description, :required_options
       
@@ -39,7 +24,7 @@ class Chef
         @description =
         'Creates a snapshot of a volume within the virtual data center. '\
         'You can use a snapshot to create a new storage volume or to restore a storage volume.'
-        @required_options = [:datacenter_id, :volume_id, :name, :ionoscloud_username, :ionoscloud_password]
+        @required_options = [:datacenter_id, :volume_id, :ionoscloud_username, :ionoscloud_password]
       end
 
       def run
@@ -53,20 +38,12 @@ class Chef
         snapshot, _, headers  = volume_api.datacenters_volumes_create_snapshot_post_with_http_info(
           config[:datacenter_id],
           config[:volume_id],
-          {
-            properties: {
-              name: config[:name],
-              description: config[:description],
-              licenceType: config[:licence_type],
-          }.compact,
-        })
+        )
 
         dot = ui.color('.', :magenta)
         api_client.wait_for { print dot; is_done? get_request_id headers }
 
-        snapshot_api = Ionoscloud::SnapshotApi.new(api_client)
-
-        snapshot = snapshot_api.snapshots_find_by_id(snapshot.id)
+        snapshot = Ionoscloud::SnapshotApi.new(api_client).snapshots_find_by_id(snapshot.id)
 
         puts "\n"
         puts "#{ui.color('ID', :cyan)}: #{snapshot.id}"
