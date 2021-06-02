593023d42db7907248b978b1998ed05362e1421c
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-06-02 12:28:29 +0300

    added server upgrade

---
+++ lib/chef/knife/ionoscloud_server_upgrade.rb
@@ -0,0 +1,43 @@
+require_relative 'ionoscloud_base'
+
+class Chef
+  class Knife
+    class IonoscloudServerUpgrade < Knife
+      include Knife::IonoscloudBase
+
+      banner 'knife ionoscloud server upgrade (options)'
+
+      option :datacenter_id,
+              short: '-D DATACENTER_ID',
+              long: '--datacenter-id DATACENTER_ID',
+              description: 'The ID of the Datacenter.'
+
+      option :server_id,
+              short: '-S SERVER_ID',
+              long: '--server-id SERVER_ID',
+              description: 'The ID of the Server.'
+
+      attr_reader :description, :required_options
+
+      def initialize(args = [])
+        super(args)
+        @description =
+        "This will upgrade the version of the server, if needed. To verify if there is an upgrade available for a server, "\
+        "call '/datacenters/{datacenterId}/servers?upgradeNeeded=true'."
+        @required_options = [:datacenter_id, :server_id, :ionoscloud_username, :ionoscloud_password]
+      end
+
+      def run
+        $stdout.sync = true
+        validate_required_params(@required_options, config)
+
+        begin
+          Ionoscloud::ServersApi.new(api_client).datacenters_servers_upgrade_post(config[:datacenter_id], config[:server_id])
+        rescue Ionoscloud::ApiError => err
+          raise err unless err.code == 404
+          ui.error("Server ID #{config[:server_id]} not found.")
+        end
+      end
+    end
+  end
+end
