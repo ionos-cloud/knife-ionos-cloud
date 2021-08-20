16d2cc82a6831d94aa356b5c74a0ee310dff6028
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-08-20 17:04:25 +0300

    tests for removing http rules

--- lib/chef/knife/ionoscloud_applicationloadbalancer_rule_httprule_remove.rb
+++ lib/chef/knife/ionoscloud_applicationloadbalancer_rule_httprule_remove.rb
@@ -5,7 +5,7 @@ class Chef
     class IonoscloudApplicationloadbalancerRuleHttpruleRemove < Knife
       include Knife::IonoscloudBase
 
-      banner 'knife ionoscloud applicationloadbalancer rule httprule remove (options)'
+      banner 'knife ionoscloud applicationloadbalancer rule httprule remove HTTP_RULE_NAME [HTTP_RULE_NAME] (options)'
 
       option :datacenter_id,
               short: '-D DATACENTER_ID',
@@ -22,19 +22,14 @@ class Chef
               long: '--forwarding-rule FORWARDING_RULE_ID',
               description: 'ID of the Application Loadbalancer Forwarding Rule'
 
-      option :name,
-              short: '-n NAME',
-              long: '--name NAME',
-              description: 'The name of a Application Load Balancer http rule; unique per forwarding rule'
-
       attr_reader :description, :required_options
 
       def initialize(args = [])
         super(args)
         @description =
-        'Removes a Http Rule from a Application Load Balancer Forwarding Rule under a data center.'
+        'Removes a Http Rule from a Application Load Balancer Forwarding Rule.'
         @required_options = [
-          :datacenter_id, :application_loadbalancer_id, :forwarding_rule_id, :name, :ionoscloud_username, :ionoscloud_password,
+          :datacenter_id, :application_loadbalancer_id, :forwarding_rule_id, :ionoscloud_username, :ionoscloud_password,
         ]
       end
 
@@ -44,36 +39,46 @@ class Chef
 
         application_loadbalancers_api = Ionoscloud::ApplicationLoadBalancersApi.new(api_client)
 
-
         application_load_balancer_rule = application_loadbalancers_api.datacenters_applicationloadbalancers_forwardingrules_find_by_forwarding_rule_id(
           config[:datacenter_id], config[:application_loadbalancer_id], config[:forwarding_rule_id],
         )
 
-        existing_http_rule = application_load_balancer_rule.properties.http_rules.nil? ? nil : application_load_balancer_rule.properties.http_rules.find do
-          |rule|
-          rule.name == config[:name]
-        end
+        if application_load_balancer_rule.properties.http_rules.nil?
+          ui.warn("The Forwarding Rule #{config[:forwarding_rule_id]} has no Http Rules.")
+        else
 
-        if existing_http_rule
-          application_load_balancer_rule.properties.http_rules = application_load_balancer_rule.properties.http_rules.reject do
+          existing_http_rules_names = application_load_balancer_rule.properties.http_rules.map { |rule| rule.name }
+
+          existing_http_rule = application_load_balancer_rule.properties.http_rules.nil? ? nil : application_load_balancer_rule.properties.http_rules.find do
             |rule|
             rule.name == config[:name]
           end
 
-          _, _, headers  = application_loadbalancers_api.datacenters_applicationloadbalancers_forwardingrules_patch_with_http_info(
-            config[:datacenter_id],
-            config[:application_loadbalancer_id],
-            config[:forwarding_rule_id],
-            Ionoscloud::ApplicationLoadBalancerForwardingRuleProperties.new({
-              http_rules: application_load_balancer_rule.properties.http_rules,
-            }),
-          )
+          valid_names = @name_args.select do |http_rule_name|
+            existing_http_rules_names.include? http_rule_name
+          end
 
-          print "#{ui.color('Removing the Http Rule to the Application Loadbalancer Forwarding Rule...', :magenta)}"
-          dot = ui.color('.', :magenta)
-          api_client.wait_for { print dot; is_done? get_request_id headers }
-        else
-          ui.warn("Http Rule #{config[:name]} not found.")
+          if valid_names.empty?
+            ui.warn('No name of an existing Http Rule was given.')
+          else
+            application_load_balancer_rule.properties.http_rules = application_load_balancer_rule.properties.http_rules.reject do
+              |rule|
+              valid_names.include? rule.name
+            end
+
+            _, _, headers  = application_loadbalancers_api.datacenters_applicationloadbalancers_forwardingrules_patch_with_http_info(
+              config[:datacenter_id],
+              config[:application_loadbalancer_id],
+              config[:forwarding_rule_id],
+              Ionoscloud::ApplicationLoadBalancerForwardingRuleProperties.new({
+                http_rules: application_load_balancer_rule.properties.http_rules,
+              }),
+            )
+
+            print "#{ui.color("Removing the Http Rules #{valid_names} from the Application Loadbalancer Forwarding Rule...", :magenta)}"
+            dot = ui.color('.', :magenta)
+            api_client.wait_for { print dot; is_done? get_request_id headers }
+          end
         end
 
         print_application_loadbalancer(application_loadbalancers_api.datacenters_applicationloadbalancers_find_by_application_load_balancer_id(
--- spec/chef/knife/ionoscloud_applicationloadbalancer_rule_httprule_add_spec.rb
+++ spec/chef/knife/ionoscloud_applicationloadbalancer_rule_httprule_add_spec.rb
@@ -12,7 +12,8 @@ describe Chef::Knife::IonoscloudApplicationloadbalancerRuleHttpruleAdd do
   end
 
   describe '#run' do
-    it 'should call ApplicationLoadBalancersApi.datacenters_applicationloadbalancers_forwardingrules_post_with_http_info' do
+    it 'should call ApplicationLoadBalancersApi.datacenters_applicationloadbalancers_forwardingrules_patch_with_http_info '\
+    'and add a new Http Rule' do
       application_loadbalancer = application_loadbalancer_mock
       application_loadbalancer_rule_httprule = application_loadbalancer_rule_httprule_mock(name: 'test')
       subject_config = {
@@ -121,6 +122,113 @@ describe Chef::Knife::IonoscloudApplicationloadbalancerRuleHttpruleAdd do
         ],
       )
 
+      expect { subject.run }.not_to raise_error(Exception)
+    end
+
+    it 'should call ApplicationLoadBalancersApi.datacenters_applicationloadbalancers_forwardingrules_patch_with_http_info '\
+    'and update an existing Http Rule' do
+      application_loadbalancer = application_loadbalancer_mock
+      application_loadbalancer_rule_httprule = application_loadbalancer_rule_httprule_mock(location: 'www.not-ionos.com', message: 'new_message')
+      subject_config = {
+        ionoscloud_username: 'email',
+        ionoscloud_password: 'password',
+        datacenter_id: 'datacenter_id',
+        application_loadbalancer_id: application_loadbalancer.id,
+        forwarding_rule_id: application_loadbalancer.entities.forwardingrules.items.first.id,
+        name: application_loadbalancer_rule_httprule.name,
+        type: application_loadbalancer_rule_httprule.type,
+        location: application_loadbalancer_rule_httprule.location,
+        response_message: application_loadbalancer_rule_httprule.response_message,
+        yes: true,
+      }
+
+      subject_config.each { |key, value| subject.config[key] = value }
+
+      forwarding_rule = application_loadbalancer.entities.forwardingrules.items.first
+
+      expected_rule_print = {
+        name: application_loadbalancer_rule_httprule.name,
+        type: application_loadbalancer_rule_httprule.type,
+        target_group: application_loadbalancer_rule_httprule.target_group,
+        drop_query: application_loadbalancer_rule_httprule.drop_query,
+        location: application_loadbalancer_rule_httprule.location,
+        status_code: application_loadbalancer_rule_httprule.status_code,
+        response_message: application_loadbalancer_rule_httprule.response_message,
+        content_type: application_loadbalancer_rule_httprule.content_type,
+        conditions: [],
+      }
+      expect(subject).to receive(:puts).with("ID: #{application_loadbalancer.id}")
+      expect(subject).to receive(:puts).with("Name: #{application_loadbalancer.properties.name}")
+      expect(subject).to receive(:puts).with("Listener LAN: #{application_loadbalancer.properties.listener_lan}")
+      expect(subject).to receive(:puts).with("IPS: #{application_loadbalancer.properties.ips}")
+      expect(subject).to receive(:puts).with("Target LAN: #{application_loadbalancer.properties.target_lan}")
+      expect(subject).to receive(:puts).with("Lb Private IPS: #{application_loadbalancer.properties.lb_private_ips}")
+      expect(subject).to receive(:puts).with("Rules: #{[{
+        id:forwarding_rule.id,
+        name: forwarding_rule.properties.name,
+        protocol: forwarding_rule.properties.protocol,
+        listener_ip: forwarding_rule.properties.listener_ip,
+        listener_port: forwarding_rule.properties.listener_port,
+        health_check: {
+          client_timeout: forwarding_rule.properties.health_check.client_timeout,
+        },
+        server_certificates: forwarding_rule.properties.server_certificates,
+        http_rules: [{
+          name: application_loadbalancer_rule_httprule.name,
+          type: application_loadbalancer_rule_httprule.type,
+          target_group: application_loadbalancer.entities.forwardingrules.items.first.properties.http_rules.first.target_group,
+          drop_query: application_loadbalancer_rule_httprule.drop_query,
+          location: application_loadbalancer_rule_httprule.location,
+          status_code: application_loadbalancer_rule_httprule.status_code,
+          response_message: application_loadbalancer_rule_httprule.response_message,
+          content_type: application_loadbalancer_rule_httprule.content_type,
+          conditions: application_loadbalancer_rule_httprule.conditions.nil? ? [] : application_loadbalancer_rule_httprule.conditions.map do |condition|
+            {
+              type: condition.type,
+              condition: condition.condition,
+              negate: condition.negate,
+              key: condition.key,
+              value: condition.value,
+            }
+          end
+        }]
+      }, subject.get_application_loadbalancer_extended_properties(application_loadbalancer)[1]]}")
+
+      expected_added_httprule_body = application_loadbalancer_rule_httprule.to_hash
+      expected_added_httprule_body[:targetGroup] = application_loadbalancer.entities.forwardingrules.items.first.properties.http_rules.first.target_group
+
+      mock_wait_for(subject)
+      mock_call_api(
+        subject,
+        [
+          {
+            method: 'GET',
+            path: "/datacenters/#{subject_config[:datacenter_id]}/applicationloadbalancers/"\
+            "#{subject_config[:application_loadbalancer_id]}/forwardingrules/#{subject_config[:forwarding_rule_id]}",
+            operation: :'ApplicationLoadBalancersApi.datacenters_applicationloadbalancers_forwardingrules_find_by_forwarding_rule_id',
+            return_type: 'ApplicationLoadBalancerForwardingRule',
+            result: application_loadbalancer.entities.forwardingrules.items.first,
+          },
+          {
+            method: 'PATCH',
+            path: "/datacenters/#{subject_config[:datacenter_id]}/applicationloadbalancers/"\
+            "#{subject_config[:application_loadbalancer_id]}/forwardingrules/#{subject_config[:forwarding_rule_id]}",
+            operation: :'ApplicationLoadBalancersApi.datacenters_applicationloadbalancers_forwardingrules_patch',
+            body: {
+              httpRules: [expected_added_httprule_body],
+            },
+            return_type: 'ApplicationLoadBalancerForwardingRule',
+          },
+          {
+            method: 'GET',
+            path: "/datacenters/#{subject_config[:datacenter_id]}/applicationloadbalancers/#{subject_config[:application_loadbalancer_id]}",
+            operation: :'ApplicationLoadBalancersApi.datacenters_applicationloadbalancers_find_by_application_load_balancer_id',
+            return_type: 'ApplicationLoadBalancer',
+            result: application_loadbalancer,
+          },
+        ],
+      )
+    
       expect { subject.run }.not_to raise_error(Exception)
     end
 
