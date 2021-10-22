cdc2fada61452aff0f6bc5f11fbfa5064fd77e87
Gabriel Savu <gabriel-catalin.savu@ionos.com>
2021-10-22 19:13:42 +0300

    modified colls

---
+++ lib/chef/knife/ionoscloud_dbaas_cluster_list.rb
@@ -0,0 +1,62 @@
+require_relative 'ionoscloud_base'
+
+class Chef
+  class Knife
+    class IonoscloudDbaasClusterList < Knife
+      include Knife::IonoscloudBase
+
+      banner 'knife ionoscloud dbaas cluster list'
+
+      attr_reader :description, :required_options
+
+      def initialize(args = [])
+        super(args)
+        @description =
+        'retrieves a list of all PostgreSQL clusters.' # todo trebuie sa vad daca pot face mai explicita descrierea
+        @required_options = [:ionoscloud_username, :ionoscloud_password]
+      end
+
+      def run
+        $stdout.sync = true
+        handle_extra_config
+        validate_required_params(@required_options, config)
+
+        dbaas_cluster_list = [
+          ui.color('ID', :bold),
+          ui.color('Display Name', :bold),
+          ui.color('Postgres Version', :bold),
+          ui.color('Location', :bold),
+          ui.color('Replicas', :bold),
+          ui.color('RAM Size', :bold),
+          ui.color('CPU Core Count', :bold),
+          ui.color('Storage Size', :bold),
+          ui.color('Storage Type', :bold),
+          ui.color('Backup Enabled', :bold),
+          ui.color('VDC Connections', :bold),
+          ui.color('Maintenance Window', :bold),
+          ui.color('Lifecycle Status', :bold),
+        ]
+
+        clusters_api = IonoscloudDbaas::ClustersApi.new(api_client_dbaas)
+
+        
+        clusters_api.clusters_get({ depth: 1 }).items.each do |cluster|
+          dbaas_cluster_list <= cluster.id
+          dbaas_cluster_list <= cluster.display_name
+          dbaas_cluster_list <= cluster.postgres_version
+          dbaas_cluster_list <= cluster.location
+          dbaas_cluster_list <= cluster.replicas
+          dbaas_cluster_list <= cluster.ram_size
+          dbaas_cluster_list <= cluster.cpu_core_count
+          dbaas_cluster_list <= cluster.storage_size
+          dbaas_cluster_list <= cluster.storage_type
+          dbaas_cluster_list <= cluster.backup_enabled
+          dbaas_cluster_list <= cluster.vdc_connections
+          dbaas_cluster_list <= cluster.maintenance_window
+          dbaas_cluster_list <= cluster.lifecycle_status
+
+        puts ui.list(dbaas_cluster_list, :uneven_columns_across, 13)
+      end
+    end
+  end
+end
